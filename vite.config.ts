a794b39a22215b39b37d00037eec9241fb2ae040
My2ndAngelic <m2a@my2ndangelic.com>
2026-07-23 10:35:05 +0200

    fix: simplify assetFileNames function in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   build: {
     rollupOptions: {
       output: {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        assetFileNames: (assetInfo) => 'assets/ba-logo-[name]-[hash][extname]',
+        assetFileNames: () => 'assets/ba-logo-[name]-[hash][extname]',
         entryFileNames: 'assets/ba-logo-[name]-[hash].js',
         chunkFileNames: 'assets/ba-logo-[name]-[hash].js',
       },
